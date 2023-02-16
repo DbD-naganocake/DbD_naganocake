f6d031c05327e62f6220732f61008cdee63a2ed3
EC2 Default User <ec2-user@ip-172-31-41-70.ap-northeast-1.compute.internal>
2023-02-16 23:13:54 +0000

    public_item

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,15 @@
 class Public::ItemsController < ApplicationController
   def index
+    @item = Item.new
+    @items = Item.page(params[:page]).per(8)
+    @genres = Genre.where(status: true)
   end
 
   def show
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :item_image, :without_tax)
   end
 end
