a951760bd8a668822ccfc52ed86b54535be3a517
EC2 Default User <ec2-user@ip-172-31-41-70.ap-northeast-1.compute.internal>
2023-02-17 08:27:35 +0000

    public_index,show

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,16 +1,25 @@
 class Public::ItemsController < ApplicationController
   def index
-    @item = Item.all
-    @items = Item.page(params[:page]).per(8)
+    @items = Item.where(sale_status: true).page(params[:page]).per(8)
     @genres = Genre.all
+      if params[:genre_id].present?
+        @genre = Genre.find(params[:genre_id])
+        @items = @genre.items.where(sale_status: true).page(params[:page]).per(8)
+      end
   end
 
   def show
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
+    @genres = Genre.all
+      if params[:genre_id].present?
+        @genre = Genre.find(params[:genre_id])
+        @items = @genre.items.where(sale_status: true).page(params[:page]).per(8)
+      end
   end
 
   private
   def item_params
-    params.require(:item).permit(:genre_id, :item_name, :item_image, :without_tax)
+    params.require(:item).permit(:item_image, :genre_id, :item_name, :item_caption, :without_tax, :sale_status)
   end
 end
