90df76a07de18738f2c419bb926f47a8211c10d3
EC2 Default User <ec2-user@ip-172-31-9-149.ap-northeast-1.compute.internal>
2023-02-18 03:54:50 +0000

    会員　マイページ　編集退会

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,9 +3,11 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :cart_items
   has_many :ordes
   has_many :deliveries
-  
+
 end
+# customer（会員）はたくさんのcart_item(注文商品),orders(注文),deliveries(配送先)を持っている
+# dependent: :destroy（親モデルを削除する際に、その親モデルに紐づく「子モデル」も一緒に削除できる）は必要？
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
 Rails.application.routes.draw do
-  
-  devise_for :admins
+
+  devise_for :admin
   devise_for :customers
   # 会員側のルーティング設定
   scope module: :public do
     root to: "homes#top"
     get 'homes/about'
-    
+
     resources :items, only: [:index,:show]
     resources :customers, only: [:edit,:show]
-    get 'customers/quit'
+    get "/customers/my_page" => "customers#show"
+    get 'customers/information/edit' => 'customers#edit'
+    patch 'customers/information' => 'customers#update'
+    get 'customers/unsubscribe' => 'customers#quit'
+    patch 'customers/withdraw' => 'customers#withdraw'
     resources :cart_items, only: [:index]
     resources :orders, only: [:new,:index,:show]
     resources :deliveries, only: [:index,:edit]
   end
-  
+
   # 管理者側のルーティング設定
   namespace :admin do
     get 'homes/top'
@@ -25,3 +29,8 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+  # get 'url' => 'コントローラー名#アクション名'
+  #1. ユーザーがRailsアプリケーションのURLにアクセスする
+  #2. そのURLに対応したコントローラとアクションを呼び出す
+  #3. そのアクションに対応したビューを表示する
+  #  syntax error 大体endが足りない,カンマがない　全角
