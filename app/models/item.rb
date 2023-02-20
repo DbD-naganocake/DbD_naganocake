b67479290b7dc990edbf2171ecbf41cf292fbb35
EC2 Default User <ec2-user@ip-172-31-41-70.ap-northeast-1.compute.internal>
2023-02-20 05:16:11 +0000

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
       file_path = Rails.root.join("app/assets/images/no_image.jpg")
       image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
     end
-    #item_image.variant(resize_to_fill: [width, height], gravity: :center).processed
+    item_image.variant(resize_to_fill: [width, height], gravity: :center).processed
   end
   
   def with_tax
