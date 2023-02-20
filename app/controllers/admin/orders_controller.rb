7f1aa4bf7b733939409f46e04b3030242b0bd7fe
EC2 Default User <ec2-user@ip-172-31-41-70.ap-northeast-1.compute.internal>
2023-02-20 12:53:53 +0000

    修正と管理注文一覧、詳細追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,27 @@
 class Admin::OrdersController < ApplicationController
+before_action :authenticate_admin!
+
+  def index
+    @orders = Order.all
+  end
+
+  def current_user_order
+    @orders = Order.where(customer_id: params[:id])
+  end
+
   def show
+    @order = Order.find(params[:id])
+    @order_lists = @order.order_details
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:order_status)
   end
 end
