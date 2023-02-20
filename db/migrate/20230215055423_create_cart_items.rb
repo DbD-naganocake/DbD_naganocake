8cd48786f4483f333d0d390cf73f8016c1f1968f
EC2 Default User <119918517+kou4869@users.noreply.github.com>
2023-02-20 05:17:11 +0000

    色々修正

--- db/migrate/20230215055423_create_cart_items.rb
+++ db/migrate/20230215055423_create_cart_items.rb
@@ -3,7 +3,7 @@ class CreateCartItems < ActiveRecord::Migration[6.1]
     create_table :cart_items do |t|
       
       t.integer :item_id,          null: false, default: ""
-      t.integer :customers_id,     null: false, default: ""
+      t.integer :customer_id,     null: false, default: ""
       t.integer :quantity,         null: false, default: ""
       t.timestamps
     end
